65ef7591ad91e4c8a2b4bb47acdf6b6686131b4c
hushuaishuai <2240962329@qq.com>
2022-01-31 00:36:46 +0800

    hu_mian  first commit

---
+++ src/main/java/com/czxy/hellowApplication.java
@@ -0,0 +1,13 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hellowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(hellowApplication.class,args);
+//        System.out.println("111111111");
+    }
+
+}
