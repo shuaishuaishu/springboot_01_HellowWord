a97fd0a96eddfdd6cbe74ee2c70c05ba6667659a
hushuaishuai <2240962329@qq.com>
2022-01-31 00:37:49 +0800

    hu_mian  second commit

--- src/main/java/com/czxy/hellowApplication.java
+++ src/main/java/com/czxy/hellowApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class hellowApplication {
     public static void main(String[] args) {
         SpringApplication.run(hellowApplication.class,args);
-//        System.out.println("111111111");
+        System.out.println("111111111");
+        System.out.println("222221111");
     }
 
 }
